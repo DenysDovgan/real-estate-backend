0cb7a35259188e32fd0af8ad3e1379ceedb266ca
Denys Dovgan <131907373+DenysDovgan@users.noreply.github.com>
2024-12-02 11:56:26 +0100

    Project init

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.0"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "pl.quickrealestate"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(23)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Spring Boot starters
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // Development tools
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // Database dependencies
+    runtimeOnly("org.postgresql:postgresql")
+    implementation("org.liquibase:liquibase-core:4.30.0")
+
+    // Mapping and utilities
+    implementation("org.mapstruct:mapstruct:1.6.3")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+    implementation("org.slf4j:slf4j-api:2.0.16")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // Spring configuration annotation processor
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    // Test dependencies
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/pl/quickrealestate/backend/RealEstateBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.quickrealestate.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateBackendApplication.class, args);
+    }
+
+}
